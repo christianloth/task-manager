d1a1b5ae4d51d6c542faeb1a3c640cf40abdf527
Reo Matsuda <reo.matsuda@gmail.com>
2022-02-21 17:20:38 -0600

    Created express middleware

---
+++ server/index.js
@@ -0,0 +1,31 @@
+// server/index.js
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+// Setting CORS to allow all connection
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+app.get('/', (req, res) => {
+    console.log('GOT')
+    res.json({test: 'HELLO'})
+})
+
+app.get('/addMember', (req, res) => {
+    // Code to write to database
+})

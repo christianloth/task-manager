b59bf615f60bbe190661a61f7d9f983afc5cb892
Reo Matsuda <reo.matsuda@gmail.com>
2022-04-06 17:07:47 -0500

    Basic server setup

--- server/index.js
+++ server/index.js
@@ -5,13 +5,15 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const apiRouter = require("./src/routes/api.routes");
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+const MainDB = require('./src/api/db')
+const DBO = new MainDB('./db/test.db')
+
+// DBO.db.run
 
 // Setting CORS to allow all connection
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
     res.header(
@@ -21,11 +23,24 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.get('/', (req, res) => {
-    console.log('GOT')
-    res.json({test: 'HELLO'})
-})
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
 
-app.get('/addMember', (req, res) => {
-    // Code to write to database
-})
+app.use("/api", apiRouter);
+
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    console.error(err.message, err.stack);
+    res.status(statusCode).json({ message: err.message });
+
+    return;
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
---
+++ server/src/api/db.js
@@ -0,0 +1,15 @@
+const sqlite3 = require('sqlite3')
+
+class MainDB {
+  constructor(dbFilePath) {
+    this.db = new sqlite3.Database(dbFilePath, (err) => {
+      if (err) {
+        console.log('Could not connect to database', err)
+      } else {
+        console.log('Connected to database')
+      }
+    })
+  }
+}
+
+module.exports = MainDB
